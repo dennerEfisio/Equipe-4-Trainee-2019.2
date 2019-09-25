30b4bcc2584f96c8db288c852b6bf0d5bf91039d
Gabriel Fernandes Silva <gabrielfsil264@gmail.com>
2019-09-24 21:59:11 -0300

    Correção da animação

--- home/js/script.js
+++ home/js/script.js
@@ -5,38 +5,12 @@ document.addEventListener("scroll", function() {
     var cont2 = document.getElementById("conteudo2");
     var cont3 = document.getElementById("conteudo3");
 
-
     var ALTURA = window.innerHeight;
     var LARGURA = window.innerWidth;
 
-    // Tela de smartphone 360px
-
-    if (LARGURA < 450) {
-        if (scrollY > ALTURA * 0.8)
-            cont2.className += " acao";
-        if (scrollY > ALTURA * 1.2)
-            cont3.className += " acao";
-    }
-    // Tela de tablet 720px
-    else if (LARGURA < 900) {
-        if (scrollY > ALTURA * 0.6)
-            cont2.className += " acao";
-        if (scrollY > ALTURA)
-            cont3.className += " acao";
-    }
-    // Tela de notebook
-    else if (LARGURA < 1050) {
-        if (scrollY > ALTURA * 0.8)
-            cont2.className += " acao";
-        if (scrollY > ALTURA * 1.2)
-            cont3.className += " acao";
-    }
-    // Tela de Computador
-    else if (LARGURA > 1050) {
-        if (scrollY > ALTURA * 0.8)
-            cont2.className += " acao";
-        if (scrollY > ALTURA * 1.8)
-            cont3.className += " acao";
-    }
+    if (scrollY + ALTURA > cont2.offsetTop)
+        cont2.className += " acao";
+    if (scrollY + ALTURA > cont3.offsetTop)
+        cont3.className += " acao";
 
 })
