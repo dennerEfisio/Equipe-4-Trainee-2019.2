132e369b5b364bb333c587d72076d8c2bb72ab94
Gabriel Fernandes Silva <gabrielfsil264@gmail.com>
2019-09-28 00:42:15 -0300

    Funcionamento do botão + detalhes e efeito de translação no carousel

--- home/js/script.js
+++ home/js/script.js
@@ -18,3 +18,64 @@ document.addEventListener("scroll", function() {
         cont3.className += " acao";
 
 })
+
+var botaoSlide1 = document.getElementById("botaoSlide1");
+var botaoSlide2 = document.getElementById("botaoSlide2");
+var botaoSlide3 = document.getElementById("botaoSlide3");
+var botaoSlide4 = document.getElementById("botaoSlide4");
+
+botaoSlide1.addEventListener("mouseenter", function() {
+
+    document.getElementById("descricao1").className += " mostraTexto";
+    botaoSlide1.innerHTML = " - Detalhes ";
+
+})
+botaoSlide1.addEventListener("mouseout", function() {
+
+    document.getElementById("descricao1").className = "descricao";
+    botaoSlide1.innerHTML = " + Detalhes ";
+
+})
+
+botaoSlide2.addEventListener("mouseenter", function() {
+
+    document.getElementById("descricao2").className += " mostraTexto";
+    botaoSlide2.innerHTML = " - Detalhes ";
+
+})
+
+botaoSlide2.addEventListener("mouseout", function() {
+
+    document.getElementById("descricao2").className = "descricao";
+    botaoSlide2.innerHTML = " + Detalhes ";
+
+})
+
+botaoSlide3.addEventListener("mouseenter", function() {
+
+    document.getElementById("descricao3").className += " mostraTexto";
+    botaoSlide3.innerHTML = " - Detalhes ";
+
+})
+
+botaoSlide3.addEventListener("mouseout", function() {
+
+    document.getElementById("descricao3").className = "descricao";
+    botaoSlide3.innerHTML = " + Detalhes ";
+
+})
+
+botaoSlide4.addEventListener("mouseenter", function() {
+
+    document.getElementById("descricao4").className += " mostraTexto";
+    botaoSlide4.innerHTML = " - Detalhes ";
+
+})
+
+botaoSlide4.addEventListener("mouseout", function() {
+
+    document.getElementById("descricao4").className = "descricao";
+    botaoSlide4.innerHTML = " + Detalhes ";
+
+
+})
