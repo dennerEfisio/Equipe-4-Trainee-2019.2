1c46a77a62e267ff6162c58330b8f2acb450f0a5
Gabriel Fernandes Silva <gabrielfsil264@gmail.com>
2019-09-28 14:19:48 -0300

    Adição de efeitos no carousel

--- home/js/script.js
+++ home/js/script.js
@@ -19,63 +19,78 @@ document.addEventListener("scroll", function() {
 
 })
 
-var botaoSlide1 = document.getElementById("botaoSlide1");
-var botaoSlide2 = document.getElementById("botaoSlide2");
-var botaoSlide3 = document.getElementById("botaoSlide3");
-var botaoSlide4 = document.getElementById("botaoSlide4");
-
-botaoSlide1.addEventListener("mouseenter", function() {
-
-    document.getElementById("descricao1").className += " mostraTexto";
-    botaoSlide1.innerHTML = " - Detalhes ";
-
-})
-botaoSlide1.addEventListener("mouseout", function() {
-
-    document.getElementById("descricao1").className = "descricao";
-    botaoSlide1.innerHTML = " + Detalhes ";
-
-})
-
-botaoSlide2.addEventListener("mouseenter", function() {
-
-    document.getElementById("descricao2").className += " mostraTexto";
-    botaoSlide2.innerHTML = " - Detalhes ";
-
-})
-
-botaoSlide2.addEventListener("mouseout", function() {
-
-    document.getElementById("descricao2").className = "descricao";
-    botaoSlide2.innerHTML = " + Detalhes ";
-
-})
-
-botaoSlide3.addEventListener("mouseenter", function() {
-
-    document.getElementById("descricao3").className += " mostraTexto";
-    botaoSlide3.innerHTML = " - Detalhes ";
-
-})
-
-botaoSlide3.addEventListener("mouseout", function() {
-
-    document.getElementById("descricao3").className = "descricao";
-    botaoSlide3.innerHTML = " + Detalhes ";
-
-})
-
-botaoSlide4.addEventListener("mouseenter", function() {
-
-    document.getElementById("descricao4").className += " mostraTexto";
-    botaoSlide4.innerHTML = " - Detalhes ";
-
-})
-
-botaoSlide4.addEventListener("mouseout", function() {
-
-    document.getElementById("descricao4").className = "descricao";
-    botaoSlide4.innerHTML = " + Detalhes ";
+function descricaoSlide() {
 
 
-})
+    var botaoSlide1 = document.getElementById("botaoSlide1");
+    var botaoSlide2 = document.getElementById("botaoSlide2");
+    var botaoSlide3 = document.getElementById("botaoSlide3");
+    var botaoSlide4 = document.getElementById("botaoSlide4");
+
+    botaoSlide1.addEventListener("mouseenter", function() {
+
+        document.getElementById("descricao1").className += " mostraTexto";
+        document.getElementById("slide1").style.filter = "blur(5px)";
+        botaoSlide1.innerHTML = " - Detalhes ";
+
+    })
+    botaoSlide1.addEventListener("mouseout", function() {
+
+        document.getElementById("descricao1").className = "descricao";
+        document.getElementById("slide1").style.filter = "blur(0)";
+        botaoSlide1.innerHTML = " + Detalhes ";
+
+    })
+
+    botaoSlide2.addEventListener("mouseenter", function() {
+
+        document.getElementById("descricao2").className += " mostraTexto";
+        document.getElementById("slide2").style.filter = "blur(5px)";
+        botaoSlide2.innerHTML = " - Detalhes ";
+
+    })
+
+    botaoSlide2.addEventListener("mouseout", function() {
+
+        document.getElementById("descricao2").className = "descricao";
+        document.getElementById("slide2").style.filter = "blur(0)";
+        botaoSlide2.innerHTML = " + Detalhes ";
+
+    })
+
+    botaoSlide3.addEventListener("mouseenter", function() {
+
+        document.getElementById("descricao3").className += " mostraTexto";
+        document.getElementById("slide3").style.filter = "blur(5px)";
+        botaoSlide3.innerHTML = " - Detalhes ";
+
+    })
+
+    botaoSlide3.addEventListener("mouseout", function() {
+
+        document.getElementById("descricao3").className = "descricao";
+        document.getElementById("slide3").style.filter = "blur(0)";
+        botaoSlide3.innerHTML = " + Detalhes ";
+
+    })
+
+    botaoSlide4.addEventListener("mouseenter", function() {
+
+        document.getElementById("descricao4").className += " mostraTexto";
+        document.getElementById("slide4").style.filter = "blur(5px)";
+        botaoSlide4.innerHTML = " - Detalhes ";
+
+    })
+
+    botaoSlide4.addEventListener("mouseout", function() {
+
+        document.getElementById("descricao4").className = "descricao";
+        document.getElementById("slide4").style.filter = "blur(0)";
+        botaoSlide4.innerHTML = " + Detalhes ";
+
+
+    })
+
+}
+
+descricaoSlide();
