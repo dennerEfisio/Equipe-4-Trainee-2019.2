e5c5409b4ed2de02b10b0de706c56b31af1d2996
Gabriel Fernandes <gabrielfsil264@gmail.com>
2019-12-01 14:11:42 -0300

    Bloqueando todos os cruds

--- app/controllers/EspetaculoController.php
+++ app/controllers/EspetaculoController.php
@@ -8,73 +8,127 @@ use app\models\Local;
 
 class EspetaculoController extends Controller{
     
-   public function index(){
-       $espetaculo =new Espetaculo();
-       $dados["espetaculos"]=$espetaculo->lista();
-       $dados["view"]="espetaculo/Index";
-       $this->load("template", $dados); 
+   public function index()
+   {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $espetaculo =new Espetaculo();
+            $dados["espetaculos"]=$espetaculo->lista();
+            $dados["view"]="espetaculo/Index";
+            $this->load("template", $dados); 
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
    }
    
-   public function novo(){
-        $categoria =new Categoria();
-        $local =new Local();
-        $dados["categorias"]=$categoria->listaCat();
-        $dados["locais"]=$local->listaLoc();
-        $dados["view"]="espetaculo/Create";
-        $this->load("template", $dados);
+   public function novo()
+   {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $categoria =new Categoria();
+            $local =new Local();
+            $dados["categorias"]=$categoria->listaCat();
+            $dados["locais"]=$local->listaLoc();
+            $dados["view"]="espetaculo/Create";
+            $this->load("template", $dados);
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
    }
 
-   public function salvar(){
-    $espetaculo =new Espetaculo();
+   public function salvar()
+    {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
 
-    $id=$_POST["id"];
-    $nome=$_POST["txt_nome"];
-    $ingresso=$_POST["txt_ingresso"];
-    $sinopse=$_POST["txt_sinopse"];
-    $url_imagem=$_POST["txt_url_imagem"];
-    date_default_timezone_set('America/Sao_Paulo');
-    $data_criacao=date('Y-m-d H:i:s');
-    $local_id=$_POST["txt_localId"];
-    $categoria_id=$_POST["txt_categoriaId"];
-
-    if($id){
-        $espetaculo->editar($id,$nome,$ingresso,$sinopse,$url_imagem,$data_criacao,$local_id,$categoria_id);
-    }else{
-        $espetaculo->inserir($nome,$ingresso,$sinopse,$url_imagem,$data_criacao,$local_id,$categoria_id);
+            
+            $espetaculo =new Espetaculo();
+        
+            $id=$_POST["id"];
+            $nome=$_POST["txt_nome"];
+            $ingresso=$_POST["txt_ingresso"];
+            $sinopse=$_POST["txt_sinopse"];
+            $url_imagem=$_POST["txt_url_imagem"];
+            date_default_timezone_set('America/Sao_Paulo');
+            $data_criacao=date('Y-m-d H:i:s');
+            $local_id=$_POST["txt_localId"];
+            $categoria_id=$_POST["txt_categoriaId"];
+        
+            if($id){
+                $espetaculo->editar($id,$nome,$ingresso,$sinopse,$url_imagem,$data_criacao,$local_id,$categoria_id);
+            }else{
+                $espetaculo->inserir($nome,$ingresso,$sinopse,$url_imagem,$data_criacao,$local_id,$categoria_id);
+            }
+            header("location:".URL_BASE."espetaculo");
+            
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
     }
-    header("location:".URL_BASE."espetaculo");
-    }
-
     public function edit($id){
-        $espetaculo =new Espetaculo(); 
-        $categoria =new Categoria();
-        $local =new Local();
-        $dados["categorias"]=$categoria->listaCat();
-        $dados["locais"]=$local->listaLoc();
-        $dados["espetaculo"]=$espetaculo->getEspetaculo($id);
-        $dados["view"]="espetaculo/Edit";
-        $this->load("template", $dados);
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $espetaculo =new Espetaculo(); 
+            $categoria =new Categoria();
+            $local =new Local();
+            $dados["categorias"]=$categoria->listaCat();
+            $dados["locais"]=$local->listaLoc();
+            $dados["espetaculo"]=$espetaculo->getEspetaculo($id);
+            $dados["view"]="espetaculo/Edit";
+            $this->load("template", $dados);
+            
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
     }
 
     public function delete($id,$excluir=NULL){
-        $espetaculo =new Espetaculo(); 
+        @session_start(); 
+        if(isset($_SESSION['status'])){
 
-        if($excluir =="S"){
-            $espetaculo->excluir($id);
-            header("location:".URL_BASE."espetaculo");
-            exit;
+            
+            $espetaculo =new Espetaculo(); 
+    
+            if($excluir =="S"){
+                $espetaculo->excluir($id);
+                header("location:".URL_BASE."espetaculo");
+                exit;
+            }
+    
+            $dados["espetaculo"]=$espetaculo->getEspetaculo($id);
+            $dados["view"]="espetaculo/Delete";
+            $this->load("template", $dados);
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
         }
-
-        $dados["espetaculo"]=$espetaculo->getEspetaculo($id);
-        $dados["view"]="espetaculo/Delete";
-        $this->load("template", $dados);
     }
 
     public function buscar(){
-        $espetaculo =new Espetaculo();
-        $pesquisa=$_GET["pesquisa"];
-        $dados["espetaculos"]=$espetaculo->busca($pesquisa);
-        $dados["view"]="espetaculo/Index";
-        $this->load("template", $dados); 
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $espetaculo =new Espetaculo();
+            $pesquisa=$_GET["pesquisa"];
+            $dados["espetaculos"]=$espetaculo->busca($pesquisa);
+            $dados["view"]="espetaculo/Index";
+            $this->load("template", $dados); 
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
     }
 }
--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -6,62 +6,115 @@ use app\models\Usuario;
 
 class UsuarioController extends Controller{
     
-   public function index(){
-       $usuario =new Usuario();
-       $dados["usuarios"]=$usuario->lista();
-       $dados["view"]="usuario/Index";
-       $this->load("template", $dados); 
+   public function index()
+   {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $usuario =new Usuario();
+            $dados["usuarios"]=$usuario->lista();
+            $dados["view"]="usuario/Index";
+            $this->load("template", $dados); 
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
    }
    
    public function novo(){
-        $dados["view"]="usuario/Create";
-       $this->load("template", $dados);
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $dados["view"]="usuario/Create";
+           $this->load("template", $dados);
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
    }
 
-   public function salvar(){
-    $usuario =new Usuario();
- 
-    $id_usuario=$_POST["id_usuario"];
-    $nome=$_POST["txt_nome"];
-    $email=$_POST["txt_email"];
-    $senha=md5($_POST["txt_senha"]);
-    $url_imagem=$_POST["txt_url_imagem"];
+   public function salvar()
+   {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
 
-
-    if($id_usuario){
-        $usuario->editar($id_usuario,$nome,$email,$senha,$url_imagem);
-    }else{
-        $usuario->inserir($nome,$email,$senha,$url_imagem);
-    }
-    header("location:".URL_BASE."usuario");
+            
+            $usuario =new Usuario();
+        
+            $id_usuario=$_POST["id_usuario"];
+            $nome=$_POST["txt_nome"];
+            $email=$_POST["txt_email"];
+            $senha=md5($_POST["txt_senha"]);
+            $url_imagem=$_POST["txt_url_imagem"];
+        
+        
+            if($id_usuario){
+                $usuario->editar($id_usuario,$nome,$email,$senha,$url_imagem);
+            }else{
+                $usuario->inserir($nome,$email,$senha,$url_imagem);
+            }
+            header("location:".URL_BASE."usuario");
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
     }
 
     public function edit($id_usuario){
-        $usuario =new Usuario(); 
-        $dados["usuario"]=$usuario->getUsuario($id_usuario);
-        $dados["view"]="usuario/Edit";
-        $this->load("template", $dados);
-    }
+        @session_start(); 
+        if(isset($_SESSION['status'])){
 
-    public function delete($id_usuario,$excluir=NULL){
-        $usuario =new Usuario(); 
-
-        if($excluir =="S"){
-            $usuario->excluir($id_usuario);
-            header("location:".URL_BASE."usuario");
-            exit;
+            $usuario =new Usuario(); 
+            $dados["usuario"]=$usuario->getUsuario($id_usuario);
+            $dados["view"]="usuario/Edit";
+            $this->load("template", $dados);
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
         }
-
-        $dados["usuario"]=$usuario->getUsuario($id_usuario);
-        $dados["view"]="usuario/Delete";
-        $this->load("template", $dados);
     }
 
-    public function buscar(){
-        $usuario =new Usuario();
-        $pesquisa=$_GET["pesquisa"];
-        $dados["usuarios"]=$usuario->busca($pesquisa);
-        $dados["view"]="usuario/Index";
-        $this->load("template", $dados); 
+    public function delete($id_usuario,$excluir=NULL)
+    {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $usuario =new Usuario(); 
+    
+            if($excluir =="S"){
+                $usuario->excluir($id_usuario);
+                header("location:".URL_BASE."usuario");
+                exit;
+            }
+    
+            $dados["usuario"]=$usuario->getUsuario($id_usuario);
+            $dados["view"]="usuario/Delete";
+            $this->load("template", $dados);
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
+    }
+
+    public function buscar()
+    {
+        @session_start(); 
+        if(isset($_SESSION['status'])){
+
+            $usuario =new Usuario();
+            $pesquisa=$_GET["pesquisa"];
+            $dados["usuarios"]=$usuario->busca($pesquisa);
+            $dados["view"]="usuario/Index";
+            $this->load("template", $dados); 
+            
+        }else{
+            $dados['view'] = "home/home";
+            $this->load("template",$dados);
+        }
     }
 }
