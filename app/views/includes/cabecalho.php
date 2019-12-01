474280fcc1063b18aa87aa406359f6daea7831c2
Gabriel Fernandes <gabrielfsil264@gmail.com>
2019-11-30 21:05:15 -0300

    Alterações no front-end do cabeçalho

--- app/views/includes/cabecalho.php
+++ app/views/includes/cabecalho.php
@@ -1,13 +1,18 @@
-<nav class="navbar navbar-expand-lg bg-dark justify-content-center">
-	<a class="navbar-brand logoSis">
-    	<img src="<?php echo URL_BASE."assets/img/Felipe_Pecas.png";?>" alt="Logo" style="width:100px">
-  	</a>
-	<ul class="navbar-nav">
-		<li class="nav-item"><a class="nav-link" href="<?php echo URL_BASE."administrador" ?>">Home</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo URL_BASE."local" ?>">Local</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo URL_BASE."categoria" ?>">Categoria</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo URL_BASE."espetaculo" ?>">Espetáculo</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo URL_BASE."usuario" ?>">Usuário</a></li>
-		<li class="nav-item"><a class="nav-link btn btn-danger" href="<?php echo URL_BASE."administrador/logout"?>">Logout</a></li>
-	</ul>
-</nav>
+<div class="parallax1">
+	<nav class="navbar navbar-expand-lg justify-content-center">
+		<a class="navbar-brand logoSis">
+			<img src="<?php echo URL_BASE."assets/img/Felipe_Pecas.png";?>" alt="Logo" width="200">
+		</a>
+		<div class="collapse navbar-collapse" id="menu">
+			<ul class="navbar-nav navFont">
+				<li class="nav-item mr-3 ml-3"><a class="nav-link text-center" href="<?php echo URL_BASE."administrador" ?>">Menu</a></li>
+				<li class="nav-item mr-3 ml-3"><a class="nav-link text-center" href="<?php echo URL_BASE."local" ?>">Local</a></li>
+				<li class="nav-item mr-3 ml-3"><a class="nav-link text-center" href="<?php echo URL_BASE."categoria" ?>">Categoria</a></li>
+				<li class="nav-item mr-3 ml-3"><a class="nav-link text-center" href="<?php echo URL_BASE."espetaculo" ?>">Espetáculo</a></li>
+				<li class="nav-item mr-3 ml-3"><a class="nav-link text-center" href="<?php echo URL_BASE."usuario" ?>">Usuário</a></li>
+			</ul>
+			<div class="navbar-nav ml-auto">
+				<a class="nav-link text-center btn btn-danger linkLogin" href="<?php echo URL_BASE."administrador/logout"?>">Logout</a>
+			</div>
+		</div>
+	</nav>
